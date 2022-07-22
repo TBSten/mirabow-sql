349db3c523ed26c87c5b2ad5774d669abdca423d
TBSten <programmingcafeteria@gmail.com>
2022-07-22 18:32:33 +0900

    [add] update文のテストを追加

--- test/update.test.ts
+++ test/update.test.ts
@@ -5,7 +5,7 @@ import { lines } from "./util";
 const matcher = updateMatcher()
 
 test.each<[string, Record<string, Tokens[]>]>([
-    //insert into values
+    //update set
     [
         lines(
             "update tbl1",
@@ -15,6 +15,21 @@ test.each<[string, Record<string, Tokens[]>]>([
         {
             "update-update": [["tbl1"]],
             "update-set": [["name", "=", "'abc'"]],
+            "where-condition": [["id", "=", "1"]],
+        },
+    ],
+    //複数列を更新
+    [
+        lines(
+            "update tbl1",
+            "set",
+            "name = 'abc',",
+            "old = old + 1",
+            "where id = 1",
+        ),
+        {
+            "update-update": [["tbl1"]],
+            "update-set": [["name", "=", "'abc'"], ["old", "=", "old", "+", "1"],],
             "where-condition": [["id", "=", "1"]],
         },
     ],
