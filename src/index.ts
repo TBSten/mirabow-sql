b1ed29db84a887d95eddf30837373d114376e0cf
TBSten <programmingcafeteria@gmail.com>
2022-09-01 18:03:53 +0900

    [fix] tsconfigを修正

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,5 @@ setConfig({
     ignoreString: /(\/\*.*\*\/|\n?--.*\n?|\s)/,
 })
 
-export * from "./statement/alterTable"
-export * from "./statement/createTable"
-export * from "./statement/delete"
-export * from "./statement/dropTable"
-export * from "./statement/insert"
-export * from "./statement/select"
-export * from "./statement/statement"
-export * from "./statement/update"
+export * from "./statement/"
 
